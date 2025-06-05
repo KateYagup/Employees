4555f12c86f404f6d1d822a7c33c58174596d625
kate <kata3140@gmail.com>
2025-06-05 10:57:37 +0300

    new

--- src/components/nothingFound/NothingFound.jsx
+++ src/components/nothingFound/NothingFound.jsx
@@ -8,7 +8,7 @@ const NothingFound = () => {
         className="nobody__picture"
         // src="/src/components/nothingFound/search.png"
         // src="../../../public/images/search.png"
-        src="/public/images/search.png"
+        src="/images/search.png"
         alt="Картинка поиск"
       />
       <div className="nobody_block">
